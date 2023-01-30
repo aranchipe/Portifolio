0b5a40e2fd2b89406818ca4c0c70a57db90884e7
unknown <aranchipe98@gmail.com>
2023-01-30 19:24:16 -0300

    deploy projeto 1

--- src/projects/projetos.js
+++ src/projects/projetos.js
@@ -1,73 +1,73 @@
-import projetoFinal from '../img/projeto-final.png'
-import cubosFlix from '../img/cubos-flix.png'
-import cubosPlayer from '../img/cubos-player.png'
-import weAreCreatives from '../img/we-are-creatives.png'
-import desafio1 from '../img/desafio1.jpg'
-import mario from '../img/mario.png'
-import dinDin from '../img/din-din.JPG'
-import menteSa from '../img/mente-sa.JPG'
+import projetoFinal from "../img/projeto-final.png";
+import cubosFlix from "../img/cubos-flix.png";
+import cubosPlayer from "../img/cubos-player.png";
+import weAreCreatives from "../img/we-are-creatives.png";
+import desafio1 from "../img/desafio1.jpg";
+import mario from "../img/mario.png";
+import dinDin from "../img/din-din.JPG";
+import menteSa from "../img/mente-sa.JPG";
 
-import projetoFinalVideo from '../videos/projeto-final.mp4'
-import cubosFlixVideo from '../videos/cubosflix.mp4'
-import dinDinVideo from '../videos/din-din.mp4'
-import cubosPlayerVideo from '../videos/cubos-player.mp4'
-import weAreCriativesVideo from '../videos/we-are-criatives.mp4'
-import desafio1Video from '../videos/desafio1.mp4'
-import marioVideo from '../videos/mario.mp4'
-import menteSaVideo from '../videos/mente-sa.mp4'
+import projetoFinalVideo from "../videos/projeto-final.mp4";
+import cubosFlixVideo from "../videos/cubosflix.mp4";
+import dinDinVideo from "../videos/din-din.mp4";
+import cubosPlayerVideo from "../videos/cubos-player.mp4";
+import weAreCriativesVideo from "../videos/we-are-criatives.mp4";
+import desafio1Video from "../videos/desafio1.mp4";
+import marioVideo from "../videos/mario.mp4";
+import menteSaVideo from "../videos/mente-sa.mp4";
 
 export const projetos = [
-    {
-        id: 1,
-        src: projetoFinal,
-        github: 'https://github.com/aranchipe/Projeto-Final-Front-end',
-        back: 'https://github.com/aranchipe/Projeto-Final-Back-End',
-        video: projetoFinalVideo,
-        site: 'https://sgf-customers.vercel.app/'
-    },
-    {
-        id: 2,
-        src: dinDin,
-        github: 'https://github.com/aranchipe/DinDin',
-        back: 'https://github.com/aranchipe/Din-Din-API',
-        video: dinDinVideo
-    },
-    {
-        id: 3,
-        src: menteSa,
-        github: 'https://github.com/aranchipe/Mente-sa-Frontend',
-        back: 'https://github.com/aranchipe/Mente-Sa-Backend',
-        video: menteSaVideo
-    },
-    {
-        id: 4,
-        src: cubosFlix,
-        github: 'https://github.com/aranchipe/Cubos-Flix',
-        video: cubosFlixVideo
-    },
-    {
-        id: 5,
-        src: cubosPlayer,
-        github: 'https://github.com/aranchipe/Cubos-Player',
-        video: cubosPlayerVideo
-    },
-    {
-        id: 6,
-        src: weAreCreatives,
-        github: 'https://github.com/aranchipe/Projetos-HTML-CSS/tree/master/WE%20ARE%20CREATIVES',
-        video: weAreCriativesVideo
-    },
-    {
-        id: 7,
-        src: desafio1,
-        github: 'https://github.com/aranchipe/Projeto-Final-Modulo-1',
-        video: desafio1Video
-    },
-    {
-        id: 8,
-        src: mario,
-        github: 'https://github.com/aranchipe/mini-game-mario-react',
-        video: marioVideo
-    }
-
-]
+  {
+    id: 1,
+    src: projetoFinal,
+    github: "https://github.com/aranchipe/desafio-final-frontend",
+    back: "https://github.com/aranchipe/desafio-final",
+    video: projetoFinalVideo,
+    site: "https://desafio-final-frontend.vercel.app/customers",
+  },
+  {
+    id: 2,
+    src: dinDin,
+    github: "https://github.com/aranchipe/DinDin",
+    back: "https://github.com/aranchipe/Din-Din-API",
+    video: dinDinVideo,
+  },
+  {
+    id: 3,
+    src: menteSa,
+    github: "https://github.com/aranchipe/Mente-sa-Frontend",
+    back: "https://github.com/aranchipe/Mente-Sa-Backend",
+    video: menteSaVideo,
+  },
+  {
+    id: 4,
+    src: cubosFlix,
+    github: "https://github.com/aranchipe/Cubos-Flix",
+    video: cubosFlixVideo,
+  },
+  {
+    id: 5,
+    src: cubosPlayer,
+    github: "https://github.com/aranchipe/Cubos-Player",
+    video: cubosPlayerVideo,
+  },
+  {
+    id: 6,
+    src: weAreCreatives,
+    github:
+      "https://github.com/aranchipe/Projetos-HTML-CSS/tree/master/WE%20ARE%20CREATIVES",
+    video: weAreCriativesVideo,
+  },
+  {
+    id: 7,
+    src: desafio1,
+    github: "https://github.com/aranchipe/Projeto-Final-Modulo-1",
+    video: desafio1Video,
+  },
+  {
+    id: 8,
+    src: mario,
+    github: "https://github.com/aranchipe/mini-game-mario-react",
+    video: marioVideo,
+  },
+];
