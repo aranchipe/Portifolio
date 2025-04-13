87e04784e1caa6378ee4dde0906004ff6a2b2cc9
Lucas Aranchipe <aranchipe98@gmail.com>
2025-04-13 19:44:20 -0300

    Adição do Safecash e do Next.js

--- src/projects/projetos.js
+++ src/projects/projetos.js
@@ -1,5 +1,5 @@
 import projetoFinal from "../img/projeto-final.png";
-import safecash from "../img/safecash.png";
+/* import safecash from "../img/safecash.png"; */
 import cubosFlix from "../img/cubos-flix.png";
 import cubosPlayer from "../img/cubos-player.png";
 import weAreCreatives from "../img/we-are-creatives.png";
@@ -9,7 +9,7 @@ import dinDin from "../img/din-din.JPG";
 import menteSa from "../img/mente-sa.JPG";
 
 import projetoFinalVideo from "../videos/projeto-final.mp4";
-import safecashVideo from "../videos/safecash.mp4";
+/* import safecashVideo from "../videos/safecash.mp4"; */
 import cubosFlixVideo from "../videos/cubosflix.mp4";
 import dinDinVideo from "../videos/din-din.mp4";
 import cubosPlayerVideo from "../videos/cubos-player.mp4";
@@ -27,55 +27,55 @@ export const projetos = [
     video: projetoFinalVideo,
     site: "https://desafio-final-frontend.vercel.app/customers",
   },
-  {
+  /* {
     id: 2,
     src: safecash,
     github: "https://github.com/aranchipe/controle-financeiro-front",
     back: "https://github.com/aranchipe/safecash-api",
     video: safecashVideo,
     site: "https://safecash.vercel.app",
-  },
+  }, */
   {
-    id: 3,
+    id: 2,
     src: dinDin,
     github: "https://github.com/aranchipe/DinDin",
     back: "https://github.com/aranchipe/Din-Din-API",
     video: dinDinVideo,
   },
   {
-    id: 4,
+    id: 3,
     src: menteSa,
     github: "https://github.com/aranchipe/Mente-sa-Frontend",
     back: "https://github.com/aranchipe/Mente-Sa-Backend",
     video: menteSaVideo,
   },
   {
-    id: 5,
+    id: 4,
     src: cubosFlix,
     github: "https://github.com/aranchipe/Cubos-Flix",
     video: cubosFlixVideo,
   },
   {
-    id: 6,
+    id: 5,
     src: cubosPlayer,
     github: "https://github.com/aranchipe/Cubos-Player",
     video: cubosPlayerVideo,
   },
   {
-    id: 7,
+    id: 6,
     src: weAreCreatives,
     github:
       "https://github.com/aranchipe/Projetos-HTML-CSS/tree/master/WE%20ARE%20CREATIVES",
     video: weAreCriativesVideo,
   },
   {
-    id: 8,
+    id: 7,
     src: desafio1,
     github: "https://github.com/aranchipe/Projeto-Final-Modulo-1",
     video: desafio1Video,
   },
   {
-    id: 9,
+    id: 8,
     src: mario,
     github: "https://github.com/aranchipe/mini-game-mario-react",
     video: marioVideo,
